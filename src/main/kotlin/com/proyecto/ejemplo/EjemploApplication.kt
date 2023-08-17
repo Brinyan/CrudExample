331b9b41beaa256698901de720209d745e2ce99e
BriyithP <briyith.prieto@soytul.com>
2023-08-17 18:40:59 -0500

    Add files

---
+++ src/main/kotlin/com/proyecto/ejemplo/EjemploApplication.kt
@@ -0,0 +1,11 @@
+package com.proyecto.ejemplo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EjemploApplication
+
+fun main(args: Array<String>) {
+	runApplication<EjemploApplication>(*args)
+}
